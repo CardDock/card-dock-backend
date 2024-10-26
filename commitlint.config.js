ffbf6c1a85c9211448bac08763806e1578b441eb
Nelson R. Graña <nelsongraa8@gmail.com>
2024-10-26 23:43:45 +0200

    chore: añade regla de longitud de línea en cuerpo del commits
    #12

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ export default {
     'header-max-length': [2, 'always', 72],
     'subject-full-stop': [2, 'never', '.'],
     'body-max-length': [2, 'always', Infinity],
+    'body-max-line-length': [2, 'always', Infinity],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'type-enum': [
