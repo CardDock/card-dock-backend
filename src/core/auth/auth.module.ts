3a2a370995c8a14460a49ee97ae5d6872208b49c
Nelson R. Graña <nelsongraa8@gmail.com>
2024-11-22 19:25:12 +0100

    fix: añade correctamente el controlador y la estrategia de google
    refer: #14

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { GoogleAuthController } from './social/controllers/google-auth.controller';
+import { GoogleStrategy } from './social/strategies/google.strategy';
 
 @Module({
 	imports: [],
-	controllers: [AuthController],
-	providers: [AuthService],
+	controllers: [AuthController, GoogleAuthController],
+	providers: [AuthService, GoogleStrategy],
 })
 export class AuthModule {}
