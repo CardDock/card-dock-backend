c5b9cb28a2ac80166e61365bb378084c32604a7e
Nelson R. Graña <nelsongraa8@gmail.com>
2024-11-22 01:42:39 +0100

    feat(ssl): configura el servidor para usar HTTPS
    refer: #14

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+/* eslint-disable no-console */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	dotenv.config();
+
+	const keyPath = path.join(__dirname, '../../certificates/key.pem');
+	const certPath = path.join(__dirname, '../../certificates/cert.pem');
+
+	if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
+    console.error('Error: No se encontraron los archivos de certificado.');
+    process.exit(1);
+  }
+
+	const httpsOptions = {
+		key: fs.readFileSync(keyPath),
+		cert: fs.readFileSync(certPath),
+	};
+
+	const app = await NestFactory.create(AppModule, { httpsOptions });
+
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
