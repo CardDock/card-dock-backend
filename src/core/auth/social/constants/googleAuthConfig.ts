5ea501f877b8ef896cf83bc84973bb2b47ca182e
Nelson R. Graña <nelsongraa8@gmail.com>
2024-11-25 01:44:32 +0100

    feat: google auth config para el strategy de google
    #16

--- src/core/auth/social/constants/googleAuthConfig.ts
+++ src/core/auth/social/constants/googleAuthConfig.ts
@@ -1,8 +1,12 @@
-export const googleAuthConfig = {
-	clientID: process.env.GOOGLE_APP_ID,
-	clientSecret: process.env.GOOGLE_APP_SECRET,
-	callbackURL:
-		process.env.GOOGLE_CALLBACK_URL ||
-		'https://localhost:3000/auth/google/redirect',
-	scope: ['email', 'profile'],
-};
+export class GoogleAuthConfig {
+	public static googleAuthStrategy() {
+		return {
+			clientID: process.env.GOOGLE_APP_ID,
+			clientSecret: process.env.GOOGLE_APP_SECRET,
+			callbackURL:
+				process.env.GOOGLE_CALLBACK_URL ||
+				'https://localhost:3000/auth/google/redirect',
+			scope: ['email', 'profile'],
+		};
+	}
+}
