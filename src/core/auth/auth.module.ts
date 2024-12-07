8c814ad11722000b631f29f829314817cdfcb760
Nelson R. Graña <nelsongraa8@gmail.com>
2024-12-08 00:46:52 +0100

    feat: agrega GoogleAuthService y PrismaService al módulo de auth
    refer: #18

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { GoogleAuthController } from './social/controllers/google-auth.controller';
 import { GoogleStrategy } from './social/strategies/google.strategy';
+import { GoogleAuthService } from './social/services/google-auth.service';
+import { PrismaService } from '../services/prisma.service';
 
 @Module({
 	imports: [],
 	controllers: [AuthController, GoogleAuthController],
-	providers: [AuthService, GoogleStrategy],
+	providers: [AuthService, GoogleStrategy, GoogleAuthService, PrismaService],
 })
 export class AuthModule {}
