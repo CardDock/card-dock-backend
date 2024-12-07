b7ebc810a73dae1d39bb282af67893cd5c78ee65
Nelson R. Graña <nelsongraa8@gmail.com>
2024-12-08 00:44:56 +0100

    feat: agregar servicio GoogleAuth para gestionar usuarios de Google
    refer: #18

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './core/auth/auth.module';
+import { PrismaService } from './core/services/prisma.service';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [AuthModule],
+	controllers: [AppController],
+	providers: [AppService, PrismaService],
+	exports: [PrismaService],
 })
 export class AppModule {}
---
+++ src/core/auth/social/services/google-auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PayloadReturnauthGoogle } from '../interfaces/payload-return-auth-google.interface';
+import { PrismaService } from 'src/core/services/prisma.service';
+
+@Injectable()
+export class GoogleAuthService {
+	constructor(private prisma: PrismaService) {}
+
+	public async searchGoogleUser(
+		user: PayloadReturnauthGoogle,
+	): Promise<unknown> {
+		return this.prisma.user.findUnique({
+			where: { email: user.email },
+		});
+	}
+
+	public async createGoogleUser(
+		user: PayloadReturnauthGoogle,
+	): Promise<unknown> {
+		return this.prisma.user.create({
+			data: { email: user.email, name: user.name },
+		});
+	}
+}
