82410d47e049a5a109f2bf3110d2e81fd5b52e80
Nelson R. Graña <nelsongraa8@gmail.com>
2024-12-08 22:40:30 +0100

    perf: elimina servicio innecesario
    refer: #18

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './core/auth/auth.module';
-import { PrismaService } from './core/services/prisma.service';
+import { AppController } from '@src/app.controller';
+import { AuthModule } from '@src/core/auth/auth.module';
+import { PrismaService } from '@src/core/services/prisma.service';
 
 @Module({
 	imports: [AuthModule],
 	controllers: [AppController],
-	providers: [AppService, PrismaService],
+	providers: [PrismaService],
 	exports: [PrismaService],
 })
 export class AppModule {}
