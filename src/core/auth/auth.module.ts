e8c2b3737bb68dfe93d7f1a954641742b31c26bd
Nelson R. Graña <nelsongraa8@gmail.com>
2024-12-29 00:25:53 +0100

    feat: importa en el modulo el dependencyInversion y el strategy
    #21

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -3,14 +3,15 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthRepository } from './infrastructure/repository/auth.repository';
 import { EmailFindService } from 'src/core/auth/application/services/email-find.service';
 import { DataBaseModule } from '../data-base/data-base.module';
-import { GoogleAuthController } from './infrastructure/controller/google-auth.controller';
-import { GoogleStrategyService } from './infrastructure/strategys/google-strategy.service';
-import { SingJwtController } from './infrastructure/controller/sing-jwt.controller';
+import { GoogleAuthController } from './infrastructure/adapters/google-auth.controller';
+import { GoogleStrategy } from './infrastructure/strategys/google.strategy';
+import { SingJwtController } from './infrastructure/adapters/sing-jwt.controller';
 import { AuthService } from './infrastructure/services/auth.service';
 import { JwtKey } from './infrastructure/constants/jwt-key';
 import { JwtStrategy } from './infrastructure/strategys/jwt.strategy';
-import { EmailFindController } from './infrastructure/controller/emailfind.controller';
+import { EmailFindController } from './infrastructure/adapters/emailfind.controller';
 import { PassportModule } from '@nestjs/passport/dist';
+import { AuthAplicationService } from './application/services/auth-aplication.service';
 
 @Module({
 	imports: [
@@ -24,10 +25,12 @@ import { PassportModule } from '@nestjs/passport/dist';
 	controllers: [GoogleAuthController, SingJwtController, EmailFindController],
 	providers: [
 		{ provide: 'AuthRepositoryPort', useClass: AuthRepository },
-		GoogleStrategyService,
+		{ provide: 'TokenSignerPort', useClass: AuthService },
+		GoogleStrategy,
 		EmailFindService,
 		AuthService,
 		JwtStrategy,
+		AuthAplicationService,
 	],
 	exports: [],
 })
