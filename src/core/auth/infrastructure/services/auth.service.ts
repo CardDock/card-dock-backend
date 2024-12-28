308c8da74af98d5c6af71b6d094f1b83577d6905
Nelson R. Graña <nelsongraa8@gmail.com>
2024-12-29 00:26:51 +0100

    perf: usa el puerto de aplicacion para validar el token jwt
    #23

--- src/core/auth/infrastructure/services/auth.service.ts
+++ src/core/auth/infrastructure/services/auth.service.ts
@@ -1,32 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { AuthEntity } from '../../domain/entitys/auth/auth.entity';
-import { AuthDto } from '../dtos/auth.dto';
-import { AuthName } from '../../domain/entitys/auth/value-object/auth-name';
-import { AuthEmail } from '../../domain/entitys/auth/value-object/auth-email';
-import { AuthPassword } from '../../domain/entitys/auth/value-object/auth-password';
 
 @Injectable()
 export class AuthService {
-	constructor(private jwtTokenService: JwtService) {}
+	constructor(
+		private jwtTokenService: JwtService,
+	) {}
 
-	async loginWithCredentials(
-		authDto: AuthDto,
-	): Promise<{ access_token: string }> {
-		const authName = new AuthName(authDto.name);
-		const authEmail = new AuthEmail(authDto.email);
-		const authPassword = new AuthPassword(authDto.password);
-
-		const authEntity = AuthEntity.create(authName, authEmail, authPassword);
-
-		const payload = {
-			name: authEntity.name,
-			email: authEntity.email,
-			password: authEntity.password,
-		};
-
-		return {
-			access_token: this.jwtTokenService.sign(payload),
-		};
+	signToken(payload: any): string {
+		return this.jwtTokenService.sign(payload);
 	}
 }
