4714ebc6730426771077ff94622d1290d3909795
Nelson R. Graña <nelsongraa8@gmail.com>
2024-12-29 14:29:39 +0100

    chore: renombra directorio de controllers
    #21

--- src/core/auth/infrastructure/controllers/emailfind.controller.ts
+++ src/core/auth/infrastructure/controllers/emailfind.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { EmailFindService } from 'src/core/auth/application/services/email-find.service';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('emailfind')
 export class EmailFindController {
 	constructor(private readonly emailFindService: EmailFindService) {}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':email')
 	async getUserByEmail(@Param('email') email: string) {
 		return this.emailFindService.findByEmail(email);
