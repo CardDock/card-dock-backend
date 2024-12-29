94cf5205d341d6c49c8b891755ab4d777fbd0595
Nelson R. Graña <nelsongraa8@gmail.com>
2024-12-29 14:30:24 +0100

    chore: corrige todas las importaciones desde el modulo
    #21

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthRepository } from './infrastructure/repository/auth.repository';
 import { EmailFindService } from 'src/core/auth/application/services/email-find.service';
 import { DataBaseModule } from '../data-base/data-base.module';
-import { GoogleAuthController } from './infrastructure/controller/google-auth.controller';
+import { GoogleAuthController } from './infrastructure/controllers/google-auth.controller';
 import { GoogleStrategy } from './infrastructure/strategys/google.strategy';
-import { SingJwtController } from './infrastructure/controller/sing-jwt.controller';
-import { AuthService } from './infrastructure/services/auth.service';
+import { SingJwtController } from './infrastructure/controllers/sing-jwt.controller';
 import { JwtKey } from './infrastructure/constants/jwt-key';
 import { JwtStrategy } from './infrastructure/strategys/jwt.strategy';
-import { EmailFindController } from './infrastructure/controller/emailfind.controller';
+import { EmailFindController } from './infrastructure/controllers/emailfind.controller';
 import { PassportModule } from '@nestjs/passport/dist';
 import { AuthAplicationService } from './application/services/auth-aplication.service';
+import { AuthAdapter } from './infrastructure/adapters/auth.adapter';
+import { AuthRepositoryAdapter } from './infrastructure/adapters/repository/auth-repository.adapter';
 
 @Module({
 	imports: [
@@ -24,12 +24,11 @@ import { AuthAplicationService } from './application/services/auth-aplication.se
 	],
 	controllers: [GoogleAuthController, SingJwtController, EmailFindController],
 	providers: [
-		{ provide: 'AuthRepositoryPort', useClass: AuthRepository },
-		{ provide: 'TokenSignerPort', useClass: AuthService },
+		{ provide: 'AuthRepositoryPort', useClass: AuthRepositoryAdapter },
+		{ provide: 'TokenSignerPort', useClass: AuthAdapter },
 		GoogleStrategy,
-		EmailFindService,
-		AuthService,
 		JwtStrategy,
+		EmailFindService,
 		AuthAplicationService,
 	],
 	exports: [],
