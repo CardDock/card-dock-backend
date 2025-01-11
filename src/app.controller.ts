1bcc6cd0a727d1c0efac6cf4d23d1de1bd5f1b58
Nelson R. Graña <nelsongraa8@gmail.com>
2025-01-11 13:35:42 +0100

    perf: elimina controlador base innecesario
    #21

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-	@Get()
-	getHello(): { msg: string } {
-		return { msg: 'Hello World!' };
-	}
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '@src/app.controller';
 import { AuthModule } from '@src/core/auth/auth.module';
 import { DataBaseModule } from './core/data-base/data-base.module';
 
 @Module({
 	imports: [AuthModule, DataBaseModule],
-	controllers: [AppController],
+	controllers: [],
 	providers: [],
 	exports: [],
 })
