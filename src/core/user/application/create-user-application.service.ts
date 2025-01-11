1f690e5cdab938740871c2aa5cf5f93464208412
Nelson R. Graña <nelsongraa8@gmail.com>
2025-01-11 14:45:37 +0100

    refactor: manejo de errores en la capa de aplicaicon
    #21

--- src/core/user/application/create-user-application.service.ts
+++ src/core/user/application/create-user-application.service.ts
@@ -2,6 +2,9 @@ import { Inject, Injectable } from '@nestjs/common';
 import { UserRepositoryPort } from '../domain/ports/user-repository.port';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserEntity } from '../domain/entity/user.entity';
+import { UserEmail } from '../domain/entity/value-objects/user-email';
+import { UserPicture } from '../domain/entity/value-objects/user-picture';
+import { UserName } from '../domain/entity/value-objects/user-name';
 
 @Injectable()
 export class CreateUserApplicationService {
@@ -12,12 +15,23 @@ export class CreateUserApplicationService {
 
 	public async create(user: CreateUserDto): Promise<any> {
 		const newUser = new UserEntity(
-			user.email,
-			user.name,
-			user.email,
-			user.picture,
+			new UserEmail(user.email),
+			new UserName(user.name),
+			new UserPicture(user.picture),
 		);
 
-		return this.userRepositoryPort.create(newUser);
+		try {
+			return this.userRepositoryPort.create(newUser);
+		} catch (error) {
+			return error;
+		}
+	}
+
+	public async findByEmail(email: string): Promise<any> {
+		try {
+			return this.userRepositoryPort.findByEmail(email);
+		} catch (error) {
+			return error;
+		}
 	}
 }
