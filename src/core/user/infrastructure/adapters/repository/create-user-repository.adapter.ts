14c0cad6886641fe821f8cb718a9d2f64ff12490
Nelson R. Graña <nelsongraa8@gmail.com>
2025-01-11 14:47:43 +0100

    refactor: buscar usario y por email de usuario
    el correctamente encargado de buscar usuarios deberia ser el modulo usuario
    
    explorar aqui la posibilidad de usar criteria para hacer busquedas de usuario por temas concretos y no un metodo para cada busqueda
    
    #21

--- src/core/user/infrastructure/adapters/repository/create-user-repository.adapter.ts
+++ src/core/user/infrastructure/adapters/repository/create-user-repository.adapter.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { DataBaseConnectService } from '@src/core/data-base/services/data-base-connect.service';
+import { DataBaseConnectService } from '@src/core/data-base/infrastructure/services/data-base-connect.service';
 import { UserEntity } from '@src/core/user/domain/entity/user.entity';
 import { UserRepositoryPort } from '@src/core/user/domain/ports/user-repository.port';
 
@@ -11,17 +11,27 @@ export class CreateUserRepositoryAdapter implements UserRepositoryPort {
 		try {
 			return await this.db.$transaction(async (tx) => {
 				const newUser = await tx.user.create({
-					data: { email: user.email, name: user.name },
+					data: { email: user.email.getValue(), name: user.name.getValue() },
 				});
 
 				await tx.userProfile.create({
 					data: {
-						portraitUser: user.picture,
+						portraitUser: user.picture.getValue(),
 						user: {
 							connect: { id: newUser.id }, // Aquí `userId` es el ID del usuario existente
 						},
 					},
 				});
+			});
+		} catch (error) {
+			return error;
+		}
+	}
+
+	async findByEmail(email: string): Promise<any> {
+		try {
+			return await this.db.user.findUnique({
+				where: { email: email },
 			});
 		} catch (error) {
 			return error;
