c901616d360cc37965bc6c7a3ea8f291bd65b28d
Nelson R. Graña <nelsongraa8@gmail.com>
2025-01-12 03:27:52 +0100

    refactor: delega en infrastructura todo a aplicacion
    #21

--- src/oauth2-provider/oauth2-provider.module.ts
+++ src/oauth2-provider/oauth2-provider.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { UserModule } from '@src/core/user/user.module';
+import { UserModule } from '@src/user/user.module';
 import { GoogleAuthController } from './infrastructure/controller/google-auth.controller';
 import { GoogleStrategy } from './infrastructure/strategy/google.strategy';
-import { CreateUserApplicationService } from '@src/core/user/application/create-user-application.service';
-import { CreateUserRepositoryAdapter } from '@src/core/user/infrastructure/adapters/repository/create-user-repository.adapter';
+import { CreateUserApplicationService } from '@src/user/application/create-user-application.service';
+import { CreateUserRepositoryAdapter } from '@src/user/infrastructure/adapters/repository/create-user-repository.adapter';
+import { GoogleValidateApplicationService } from './application/service/google-validate-aplication.service';
 
 @Module({
 	imports: [PassportModule, UserModule],
@@ -13,6 +14,10 @@ import { CreateUserRepositoryAdapter } from '@src/core/user/infrastructure/adapt
 		GoogleStrategy,
 		CreateUserApplicationService,
 		{ provide: 'UserRepositoryPort', useClass: CreateUserRepositoryAdapter },
+		{
+			provide: 'GoogleValidatePort',
+			useClass: GoogleValidateApplicationService,
+		},
 	],
 	exports: [],
 })
