fcb27a2fa379b46012dc395470deee441eb0090e
Nelson R. Graña <nelsongraa8@gmail.com>
2025-01-25 03:17:28 +0100

    refactor: mejora encapsulamiento en AuthEntity y corrige nombre de evento
    #21

--- src/core/auth/domain/entitys/auth/auth.entity.ts
+++ src/core/auth/domain/entitys/auth/auth.entity.ts
@@ -1,24 +1,33 @@
 import { AggregateDomain } from '@src/shared/domain/aggregate/aggregate-domain';
 import { AuthPassword } from './value-object/auth-password';
-import { AuthCreateDomainEvent } from './events/auth-create-domain.even';
+import { AuthCreateDomainEvent } from './events/auth-create-domain.event';
 import { AuthUsername } from './value-object/auth-name';
 
 export class AuthEntity extends AggregateDomain {
-	constructor(
-		public readonly username: AuthUsername,
-		public readonly password: AuthPassword,
+	private constructor(
+		private readonly _username: AuthUsername,
+		private readonly _password: AuthPassword,
 	) {
 		super();
 	}
 
-	static create(usernameDto: string, passwordDto: string): AuthEntity {
-		const username = new AuthUsername(usernameDto);
-		const password = new AuthPassword(passwordDto);
+	static create(username: string, password: string): AuthEntity {
+		const usernameInstance = new AuthUsername(username);
+		const passwordInstance = new AuthPassword(password);
 
-		const authEntity = new AuthEntity(username, password);
+		const authEntity = new AuthEntity(usernameInstance, passwordInstance);
+
 		authEntity.registerCreationEvent();
 
 		return authEntity;
+	}
+
+	get username(): AuthUsername {
+		return this.username;
+	}
+
+	get password(): AuthPassword {
+		return this.password;
 	}
 
 	private registerCreationEvent(): void {
