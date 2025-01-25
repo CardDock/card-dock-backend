7328c264beed70614fa84ea6ec379a133110bc77
Nelson R. Graña <nelsongraa8@gmail.com>
2025-01-25 03:29:43 +0100

    chore: ajustes minimos de configuracion y formato

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  singleQuote: true, // Usa comillas simples en lugar de comillas dobles para las cadenas de texto
-  trailingComma: 'all', // Añade una coma al final de los elementos en objetos, arrays, etc.
-  quoteProps: 'consistent', // Usa comillas para todas las propiedades si al menos una propiedad requiere comillas
-  semi: true, // Añade punto y coma al final de cada declaración
-  useTabs: true, // Usa espacios en lugar de tabulaciones
-  tabWidth: 2, // Establece el ancho del tabulador en 2 espacios
-  arrowParens: 'always', // Siempre incluye paréntesis alrededor de los parámetros de las funciones flecha
-  bracketSpacing: true, // Añade espacios dentro de los corchetes de los objetos
-  endOfLine: 'crlf', // Usa LF (line feed) como el carácter de fin de línea
-  proseWrap: 'preserve', // Mantiene el formato original del texto en los comentarios y cadenas de texto largas
-  embeddedLanguageFormatting: 'auto', // Formatea automáticamente el código embebido en otros lenguajes
+	singleQuote: true, // Usa comillas simples en lugar de comillas dobles para las cadenas de texto
+	trailingComma: 'all', // Añade una coma al final de los elementos en objetos, arrays, etc.
+	quoteProps: 'consistent', // Usa comillas para todas las propiedades si al menos una propiedad requiere comillas
+	semi: true, // Añade punto y coma al final de cada declaración
+	useTabs: true, // Usa espacios en lugar de tabulaciones
+	tabWidth: 2, // Establece el ancho del tabulador en 2 espacios
+	arrowParens: 'always', // Siempre incluye paréntesis alrededor de los parámetros de las funciones flecha
+	bracketSpacing: true, // Añade espacios dentro de los corchetes de los objetos
+	endOfLine: 'crlf', // Usa LF (line feed) como el carácter de fin de línea
+	proseWrap: 'preserve', // Mantiene el formato original del texto en los comentarios y cadenas de texto largas
+	embeddedLanguageFormatting: 'auto', // Formatea automáticamente el código embebido en otros lenguajes
+	bracketSameLine: true, // Coloca las llaves de apertura en la misma línea que la declaración
 };
