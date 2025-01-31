f32507902aad6b716750949b7b83bb555c87d427
Nelson R. Graña <nelsongraa8@gmail.com>
2025-01-31 13:55:00 +0100

    refactor: icrea entidad de dominio profile

--- src/oauth2-provider/application/service/google-validate-aplication.service.ts
+++ src/oauth2-provider/application/service/google-validate-aplication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserApplicationService } from '@src/user/application/create-user-application.service';
 import { CreateUserDto } from '@src/user/application/dtos/create-user.dto';
 import { GoogleValidatePort } from '../ports/google-validate.port';
+import { ProfileEntity } from '@src/oauth2-provider/domain/profile/entity/profile.entity';
 
 @Injectable()
 export class GoogleValidateApplicationService implements GoogleValidatePort {
@@ -9,18 +10,23 @@ export class GoogleValidateApplicationService implements GoogleValidatePort {
 		private readonly createUserApplicationService: CreateUserApplicationService,
 	) {}
 
-	async handleGoogleValidate(profile: any) {
-		const { email, displayName, picture } = profile;
+	async handleGoogleValidate(profile: any): Promise<unknown> {
+		const profileEntity = ProfileEntity.create(
+			profile.email,
+			profile.displayName,
+			profile.picture,
+		);
 
-		const existingUser =
-			await this.createUserApplicationService.findByEmail(email);
+		const existingUser = await this.createUserApplicationService.findByEmail(
+			profileEntity.email,
+		);
 
 		if (existingUser) return null;
 
 		const userCreate: CreateUserDto = {
-			name: displayName,
-			email: email,
-			picture: picture,
+			name: profileEntity.name,
+			email: profileEntity.email,
+			picture: profileEntity.picture,
 		};
 
 		return this.createUserApplicationService.create(userCreate);
---
+++ src/oauth2-provider/domain/profile/entity/profile.entity.ts
@@ -0,0 +1,30 @@
+export class ProfileEntity {
+	constructor(
+		private readonly _email: string,
+		private readonly _name: string,
+		private readonly _picture: string,
+	) {}
+
+	static create(email: string, name: string, picture: string) {
+		const profile = new ProfileEntity(email, name, picture);
+
+		// * Crear un evento de dominio de que se ha creado una entidad de usuario
+
+		return profile;
+	}
+
+	get email(): string {
+		return this.email;
+	}
+
+	get name(): string {
+		return this.name;
+	}
+
+	get picture(): string {
+		return this.picture;
+	}
+}
+
+// * TODO annadir en esta entidad los value objects de la entidad
+// * TODO pensar un cambio para cuando necesite facebook y creascan los numeros de oauths2
