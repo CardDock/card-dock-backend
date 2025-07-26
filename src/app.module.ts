b746f4d4d189bceb80f6ebece46222eb757240d5
Nelson R. Graña <nelsongraa8@gmail.com>
2025-07-26 22:09:37 +0200

    feat: elimina importacion de antiguos modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '@src/core/auth/auth.module';
 import { UserModule } from './user/user.module';
-import { Oauth2ProviderModule } from './oauth2-provider/oauth2-provider.module';
 
 @Module({
-	imports: [AuthModule, Oauth2ProviderModule, UserModule],
+	imports: [UserModule],
 	controllers: [],
 	providers: [],
 	exports: [],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CreateUserApplicationService } from './application/create-user-application.service';
-import { CreateUserRepositoryAdapter } from './infrastructure/adapters/repository/create-user-repository.adapter';
 import { DataBaseModule } from '@src/core/data-base/data-base.module';
 
 @Module({
 	imports: [DataBaseModule],
 	controllers: [],
-	providers: [
-		{ provide: 'UserRepositoryPort', useClass: CreateUserRepositoryAdapter },
-		CreateUserApplicationService,
-		CreateUserRepositoryAdapter,
-	],
+	providers: [],
 })
 export class UserModule {}
